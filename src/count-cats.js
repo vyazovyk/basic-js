00bc39bf083e38fe0b31eb8e85971d8e80004156
Kostya Vyazovyk <vyazovyk@gmail.com>
2021-03-28 23:55:59 +0300

    feat: add dreamteam solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,7 +4,7 @@ module.exports = function countCats( matrix ) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
   let count = 0;
-  for(let i = 0; i < matrix.lenght; i++) {
+  for(let i = 0; i < matrix.length; i++) {
     let depth = matrix[i];
     for(let j = 0; j < depth.length; j++) {
       if(depth[j] == '^^') {
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+// //const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  members = members.filter(e => typeof e === 'string' && e !== '');
+  if (members) {  // && e !== ''
+    for (var i = 0; i < members.length; i++) {
+      members[i] = members[i].replace(/\s/g, '').slice(0,1);
+    }
+  }
+  return members.sort().join('').toUpperCase();
 };
+
+
+// let members = ['     Olivia', 1111, 'Lily', 'Oscar', true, null];
+// members;
+// members = members.filter(e => typeof e === 'string' && e !== '');
+// members;
+
+// for (var i = 0; i < members.length; i++) {
+//   members[i] = members[i].replace(/\s/g, '').slice(0,1);
+// }
+
+// members;
+// members = members.sort();
+// members;
+
+// members = members.join('');
+// members;
+
