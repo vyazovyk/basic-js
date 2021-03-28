f13a43c5b3440fa36d6eb1b4c1fabfba2fd1a1af
Kostya Vyazovyk <vyazovyk@gmail.com>
2021-03-28 15:18:10 +0300

    feat: add counCats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats( matrix ) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  for(let i = 0; i < matrix.lenght; i++) {
+    let depth = matrix[i];
+    for(let j = 0; j < depth.length; j++) {
+      if(depth[j] == '^^') {
+        count++;
+      }
+    }
+  }
+  return count;
 };
